391cf37daabc7cba1a742a59ce7a87230d93de4a
abhay1198 <abhayagarwal1198@gmail.com>
2022-09-27 16:37:57 +0530

    product attribute

---
+++ Abhay/CustomAttribute/Setup/Patch/Data/ProductAttribute.php
@@ -0,0 +1,92 @@
+<?php
+
+/**
+ * @package     Abhay/CustomAttribute
+ * @version     1.0.0
+ * @author      Abhay
+ * @copyright   Copyright © 2021. All Rights Reserved.
+ */
+
+namespace Abhay\CustomAttribute\Setup\Patch\Data;
+
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+
+class ProductAttribute implements DataPatchInterface
+{
+     /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+    /**
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    /**
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param EavSetupFactory $eavSetupFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        EavSetupFactory $eavSetupFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply()
+    {
+        /** @var EavSetup $eavSetup */
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $eavSetup->addAttribute(
+            \Magento\Catalog\Model\Product::ENTITY,
+            'product_cart_limit',
+            [
+                'type' => 'varchar',
+                'backend' => '',
+                'frontend' => '',
+                'label' => 'Product Purchase Limit for Customer',
+                'input' => 'text',
+                'class' => '',
+                'source' => '',
+                'global' => ScopedAttributeInterface::SCOPE_STORE,
+                'visible' => true,
+                'required' => false,
+                'user_defined' => false,
+                'default' => '',
+                'searchable' => false,
+                'filterable' => false,
+                'comparable' => false,
+                'visible_on_front' => false,
+                'used_in_product_listing' => false,
+                'unique' => false,
+                'apply_to' => 'simple,configurable,bundle',
+                'frontend_class'=>'validate-zero-or-greater',
+                'note' => 'Not applicable on downloadable and virtual product.'
+            ]
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+}
